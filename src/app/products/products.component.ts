c0e940b152ca449599215e1d3424c0fe7d3e236c
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-04-03 15:37:22 -0300

    Componente Products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,6 +10,49 @@ export class ProductsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //RESULTADO SEGÚN LA SELECCIÓN DEL USUARIO
+    let muebles = document.querySelector('.muebles')
+    let cocinas = document.querySelector('.cocinas')
+    let terrazas = document.querySelector('.terrazas')
+    let iluminacion = document.querySelector('.iluminacion')
+    let dormitorio = document.querySelector('.dormitorio')
+    let alfombras = document.querySelector('.alfombras')
+    let ninos = document.querySelector('.ninos')
+
+    let menuProducts = document.querySelector('.menuProducts')
+
+    console.log(this)
+    muebles.addEventListener('click', function () {
+      
+    })
+
+    cocinas.addEventListener('click', function () {
+
+    })
+
+    terrazas.addEventListener('click', function () {
+
+    })
+
+    iluminacion.addEventListener('click', function () {
+
+    })
+
+    dormitorio.addEventListener('click', function () {
+
+    })
+
+    alfombras.addEventListener('click', function () {
+
+    })
+
+    ninos.addEventListener('click', function () {
+
+    })
+
+
+
+
   }
 
 }
