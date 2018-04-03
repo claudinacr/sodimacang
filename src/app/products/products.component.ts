a536eae64b308a3868e5b780b2db2e8120781674
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-04-03 17:39:44 -0300

    API Mercado Libre

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,56 +12,346 @@ export class ProductsComponent implements OnInit {
   ngOnInit() {
     //RESULTADO SEGÚN LA SELECCIÓN DEL USUARIO
     let muebles = document.querySelector('.muebles')
-    let cocinas = document.querySelector('.cocinas')
-    let terrazas = document.querySelector('.terrazas')
-    let iluminacion = document.querySelector('.iluminacion')
-    let dormitorio = document.querySelector('.dormitorio')
-    let alfombras = document.querySelector('.alfombras')
-    let ninos = document.querySelector('.ninos')
+    let accesorios = document.querySelector('.accesorios')
+    let electrodomesticos = document.querySelector('.electrodomesticos')
+    let herramientas = document.querySelector('.herramientas')
+    let industrias = document.querySelector('.industrias')
+    let computacion = document.querySelector('.computacion')
 
     let menuProducts = document.querySelector('.menuProducts')
 
-  
     muebles.addEventListener('click', function () {
-      fetch('https://api.mercadolibre.com/sites/MLC/categories', {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
-})
-  .then(function(respt) {
-    return respt.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC1574', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+
+          let container = document.querySelector('.container');
+          container.innerHTML='';
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+
+
+        })
     })
 
-   /*  cocinas.addEventListener('click', function () {
+    accesorios.addEventListener('click', function () {
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC1747', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let container = document.querySelector('.container');
+          container.innerHTML='';
 
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+
+        })
     })
 
-    terrazas.addEventListener('click', function () {
+    electrodomesticos.addEventListener('click', function () {
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC5726', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let container = document.querySelector('.container');
+          container.innerHTML='';
 
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+        })
     })
 
-    iluminacion.addEventListener('click', function () {
+    herramientas.addEventListener('click', function () {
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC178483', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let container = document.querySelector('.container');
+          container.innerHTML='';
 
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+        })
     })
 
-    dormitorio.addEventListener('click', function () {
+    industrias.addEventListener('click', function () {
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC1499', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let container = document.querySelector('.container');
+          container.innerHTML='';
 
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+        })
     })
 
-    alfombras.addEventListener('click', function () {
+    computacion.addEventListener('click', function () {
+      fetch('https://api.mercadolibre.com/sites/MLC/search?category=MLC1648', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(function (respt) {
+          return respt.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let container = document.querySelector('.container');
+          container.innerHTML='';
 
+          for (let i = 0; i < data.results.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            box.style.border = '1px blue solid';
+            box.style.background = 'pink';
+            box.style.width = '15em';
+            box.style.display = 'flex';
+            box.style.flexDirection = 'column'
+            box.style.marginBottom = '2em';
+            box.style.justifyContent = 'space-between';
+            box.style.height = '15em';
+            box.style.textAlign = 'center';
+
+
+
+
+
+            let title = document.createElement('div');
+            let image = document.createElement('div');
+            let price = document.createElement('div');
+
+            title.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].title)))
+            let imageSRC = document.createElement('img');
+            imageSRC.setAttribute('src', data.results[i].thumbnail)
+            price.appendChild(document.createElement('p').appendChild(document.createTextNode(data.results[i].price)))
+            image.appendChild(imageSRC)
+            box.appendChild(title)
+            box.appendChild(image)
+            box.appendChild(price)
+            container.appendChild(box)
+
+
+
+
+          }
+        })
     })
 
-    ninos.addEventListener('click', function () {
 
-    })
- */
+
+
+
+
 
 
 
